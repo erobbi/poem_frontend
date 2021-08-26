c2307cc2caddbebaedf6ac9ea7b342773a3b5493
Daniel <dannyblaze469@gmail.com>
2021-08-26 16:36:08 -0400

    fixed search

--- src/components/PoemCard.js
+++ src/components/PoemCard.js
@@ -5,6 +5,7 @@ import CommentForm from './CommentForm'
 import CommentSection from './CommentSection'
 import PoemRender from "./PoemRender"
 import { useState } from 'react';
+import {Link} from "react-router-dom"
 
 function PoemCard({ selectPoem, authorData }) {
 
@@ -21,10 +22,11 @@ function PoemCard({ selectPoem, authorData }) {
     return (
         <div> 
             <div id="nav-bar-container" class="ui container">
+            <h1>FLAT.POEM</h1>
             <div id="nav-bar" className="ui three item menu">
                 <a onClick={renderPoemForm} className="item">New Poem</a>
                 <a onClick={renderPoem} className="item">Poem</a>
-                <a className="item">Log-Out</a>
+                <a className="item"><Link style={{ color: "black"}} to="/">Log-Out</Link></a>
             </div>
             </div>
             {!isNewPoem ? <PoemRender selectPoem={selectPoem} authorData={authorData}/> : <NewPoemForm/> }
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,8 +1,20 @@
 import SearchBar from "./SearchBar";
 import PoemContainer from "./PoemsContainer";
 import SideBarPoemItem from "./SideBarPoemItem";
+import { useState } from 'react'
 
 function Sidebar({poemData, setSelectPoem}) {
+  const [search, setSearch] = useState("");
+
+  const searchItems = poemData.filter((poem) => {
+    return poem.title.toLowerCase().includes(search.toLowerCase())
+  })
+
+  let poem = searchItems || poemData
+
+  function handleChange(e) {
+    setSearch(e.target.value);
+  }
 
   return (
     <div id="side-bar">
@@ -10,13 +22,12 @@ function Sidebar({poemData, setSelectPoem}) {
         <div id="sidebar" className="ui left fixed vertical menu">
           <div className="item">
             <div className="ui transparent icon input">
-              <input type="text" placeholder="Search Poems..." />
+              <input type="text" placeholder="Search Poems..." onChange={handleChange} />
               <i className="search icon"></i>
             </div>
           </div>
 
-          {/* return list here */}
-          {poemData.map((eachPoem) => <SideBarPoemItem key={eachPoem.id} setSelectPoem={setSelectPoem} eachPoem={eachPoem} />)}
+          {poem.map((eachPoem) => <SideBarPoemItem key={eachPoem.id} setSelectPoem={setSelectPoem} eachPoem={eachPoem} />)}
           
           <PoemContainer />
         </div>
